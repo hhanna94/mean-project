4441b5fcd6518018b07cd4dfdabf4de9c6156e35
hhanna94 <heatherhanna1994@gmail.com>
2022-01-06 12:37:30 -0500

    updated create, added delete for posts

--- server/app.js
+++ server/app.js
@@ -23,18 +23,25 @@ app.use((req, res, next) => {
 
 app.get('/api/posts', (req, res, next) => {
   Post.find()
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-
+    .then(posts => res.status(200).json(posts))
 })
 
 app.post('/api/posts', (req, res, next) => {
   const post = new Post(req.body);
   // Insert the post into the database
-  post.save();
-  // Typical status code for something new being created
-  res.status(201).json({message: "Post added successfully."});
+  post.save()
+    .then( savedPost => {
+      res.status(201).json({message: "Post added successfully.", postId: savedPost._id});
+    })
+})
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id})
+    .then(result => {
+      console.log(result)
+      res.status(200).json({message: "Post deleted."});
+    })
+    .catch(err => console.log(err))
 })
 
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,11 +36,25 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
       .subscribe((res) => {
-        console.log(res.message);
+        // Add the returned post ID to the post so that the DOM actually have an ID for the post without reloading..
+        post.id = res.postId;
+        // Add the post to the post list.
         this.posts.push(post);
+        // Update the DOM with the updated post list
         this.postsUpdated.next([...this.posts]);
       })
+  }
+
+  deletePost(postId: string) {
+    this.http.delete(`http://localhost:3000/api/posts/${postId}`)
+      .subscribe( () => {
+        // Delete the post from the post list
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts
+        // Update the DOM with the updated post list
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
